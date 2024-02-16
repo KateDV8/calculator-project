79425de9bc224a74f50b66a470abc7086727aa80
KateDV8 <pavlovae8@gmail.com>
2024-02-16 12:22:17 +0500

    Calculator

---
+++ src/main/java/ru/itgirls/calcapp/_main/_Main.java
@@ -0,0 +1,12 @@
+import ru.itgirls.calcapp.service.ResultWriterService;
+import ru.itgirls.calcapp.util.Calculator;
+
+
+public class _Main {
+
+    public static void main(String[] args) {
+        ResultWriterService.printResult();
+
+    }
+
+}
---
+++ src/main/java/ru/itgirls/calcapp/util/Calculator.java
@@ -0,0 +1,19 @@
+package ru.itgirls.calcapp.util;
+
+public class Calculator {
+    public static int calculateSum(int num1, int num2) {
+        int sum = num1 + num2;
+        return sum;
+
+    }
+    public static int calculateDifference(int num1, int num2) {
+        int difference = num1 - num2;
+        return difference;
+
+    }
+    public static int calculateMultiply(int num1, int num2) {
+        int multiply = num1 * num2;
+        return multiply;
+
+    }
+}
